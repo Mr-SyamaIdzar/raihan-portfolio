c96919aa283a28e923fd689be7b67d53148bcbf2
Mr. Syamaidzar <muhammadraihansyamaidzar@gmail.com>
2025-07-12 21:49:17 +0700

    @theme customization

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen ">
+      {/* Theme Toggle */}
+      {/* Background Effect */}
+      {/* Navbar */}
+      {/* Main Content */}
+      {/* Footer */}
+      Home
+    </div>
+  );
+};
