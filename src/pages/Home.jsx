3c7c1293fe089b954db2579d73a23166a8f83b8a
Mr. Syamaidzar <muhammadraihansyamaidzar@gmail.com>
2025-07-13 21:36:20 +0700

    Theme toggle

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { ThemeToggle } from "../components/ThemeToggle";
 
 export const Home = () => {
   return (
-    <div className="min-h-screen ">
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* Theme Toggle */}
+      <ThemeToggle />
       {/* Background Effect */}
       {/* Navbar */}
       {/* Main Content */}
       {/* Footer */}
-      Home
     </div>
   );
 };
