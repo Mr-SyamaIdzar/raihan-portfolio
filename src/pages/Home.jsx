1a333378b4fa2ec0936b045e1da980989dba5203
Mr. Syamaidzar <muhammadraihansyamaidzar@gmail.com>
2025-07-13 22:01:20 +0700

    Background effect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground";
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* Theme Toggle */}
       <ThemeToggle />
+
       {/* Background Effect */}
+      <StarBackground />
       {/* Navbar */}
       {/* Main Content */}
       {/* Footer */}
