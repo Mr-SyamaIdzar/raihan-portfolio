2281c016199fcef9d23e1a7042d0bee52a6df995
Mr. Syamaidzar <muhammadraihansyamaidzar@gmail.com>
2025-07-14 21:58:22 +0700

    Navbar components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
+import { Navbar } from "../components/Navbar";
 
 export const Home = () => {
   return (
@@ -11,6 +12,7 @@ export const Home = () => {
       {/* Background Effect */}
       <StarBackground />
       {/* Navbar */}
+      <Navbar />
       {/* Main Content */}
       {/* Footer */}
     </div>
